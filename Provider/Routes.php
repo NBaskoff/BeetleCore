f707f7c21415f8486b1456a3aa23df9255b9ee07
Nikolay <nbaskoff@gmail.com>
2020-09-03 14:57:16 +0700

    fix routes

--- Provider/Routes.php
+++ Provider/Routes.php
@@ -1,13 +1,13 @@
 <?php
 
-
 namespace BeetleCore\Provider;
 
+use Illuminate\Support\ServiceProvider;
 
-class Routes
+class Routes extends ServiceProvider
 {
 	public function boot()
 	{
-		include __DIR__.'/../routes/web.php';
+		include __DIR__ . '/../routes/web.php';
 	}
 }
