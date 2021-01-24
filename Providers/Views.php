595c3bd795363a49f673cae638a9413a24ed9f92
NBaskoff <nbaskoff@gmail.com>
2021-01-24 21:15:07 +0700

    relation noempty fix

--- Providers/Views.php
+++ Providers/Views.php
@@ -2,15 +2,15 @@
 
 
 namespace BeetleCore\Providers;
-use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
 
 class Views extends ServiceProvider
 {
-	public function boot()
-	{
-		$this->loadViewsFrom(__DIR__ . "/../resources/views", "beetlecore");
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . "/../resources/views", "beetlecore");
         //view()->share("beetleCoreResourcesFolder", "/resources");
-        view()->share("beetleCoreResourcesFolder", "//cdn.jsdelivr.net/gh/NBaskoff/BeetleCore@1.9.1/resources");
-	}
+        view()->share("beetleCoreResourcesFolder", "//cdn.jsdelivr.net/gh/NBaskoff/BeetleCore@1.9.2/resources");
+    }
 }
--- resources/views/fields/Relation.blade.php
+++ resources/views/fields/Relation.blade.php
@@ -1,5 +1,5 @@
 @if ($action == "find")
-    <div class="form-group relation-form-box"  data-ignore-id="{{$ignoreId}}" data-multiple="{{$multiple}}" data-field="{{$class->field}}" data-model="{{get_class($model)}}" data-link-self="{{$model->getLinkSelf()}}" data-action="{{$action}}">
+    <div class="form-group relation-form-box" data-ignore-id="{{$ignoreId}}" data-multiple="{{$multiple}}" data-field="{{$class->field}}" data-model="{{get_class($model)}}" data-link-self="{{$model->getLinkSelf()}}" data-action="{{$action}}">
         <label class="col-form-label">
             {{$class->name}}
             <span class="relation-add-edit" style="cursor: pointer;">
@@ -27,18 +27,20 @@
     <div class="form-group row">
         <label class="col-md-4 col-form-label">{{$class->name}}</label>
         <div class="col-md-8 relation-form-box" data-ignore-id="{{$ignoreId}}" data-multiple="{{$multiple}}" data-field="{{$class->field}}" data-model="{{get_class($model)}}" data-link-self="{{$model->getLinkSelf()}}" data-action="{{$action}}">
-            <div class="relation-ids">
-                @foreach($ids as $id)
-                    <div class="relation-id" data-id="{{$id["id"]}}">
-                        <input type="hidden" name="{{$class->field}}[id][]" value="{{$id["id"]}}">
-                        {{$id["name"]}}
-                        <div class="close">
-                            <i class="fas fa-times-circle"></i>
+            <div class="form-control" name="{{$class->field}}" style="height: auto;">
+                <div class="relation-ids">
+                    @foreach($ids as $id)
+                        <div class="relation-id" data-id="{{$id["id"]}}">
+                            <input type="hidden" name="{{$class->field}}[id][]" value="{{$id["id"]}}">
+                            {{$id["name"]}}
+                            <div class="close">
+                                <i class="fas fa-times-circle"></i>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
+                <div class="btn btn-primary btn-sm relation-add-edit">Добавить / Удалить</div>
             </div>
-            <div class="btn btn-primary btn-sm relation-add-edit">Добавить / Удалить</div>
             <div class="invalid-feedback invalid-feedback-{{$class->field}}">
                 @foreach($class->errors as $k=>$i)
                     {{$i}}<br>
