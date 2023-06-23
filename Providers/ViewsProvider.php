565710bf0381c7b180c29d161149e45718280293
Nikolay <nbaskoff@gmail.com>
2023-06-23 07:37:26 +0700

    Update ViewsProvider.php

--- Providers/ViewsProvider.php
+++ Providers/ViewsProvider.php
@@ -13,6 +13,6 @@ class ViewsProvider extends ServiceProvider
         Paginator::useBootstrap();
         $this->loadViewsFrom(__DIR__ . "/../resources/views", "beetlecore");
         //view()->share("beetleCoreResourcesFolder", "/resources");
-        view()->share("beetleCoreResourcesFolder", "//cdn.jsdelivr.net/gh/NBaskoff/BeetleCore@1.9.13/resources");
+        view()->share("beetleCoreResourcesFolder", "//cdn.jsdelivr.net/gh/NBaskoff/BeetleCore@1.9.14/resources");
     }
 }
